bc169253d585c9953238fd6fc35826ac14feb855
Alondra Covarrubias <acovarrubias10@ucol.mx>
2023-01-26 23:10:44 -0600

    Mi primer componente en react

---
+++ 03-counter-app-vite/src/VaneApp.jsx
@@ -0,0 +1,15 @@
+//function App() {
+//    return (<h1>Hola mundo</h1>
+//    );
+//}
+//
+//export default App;
+
+//export const VaneApp = () => {
+//  return (
+//    <h3>Hello VaneApppp</h3>
+//  );
+//}
+
+
+export const VaneApp = () => <h1>Arrow function</h1>;
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom/client'
+import { VaneApp } from './VaneApp';
 
-function App() {
-    //document,createElement......
-    return (<h1>Hola mundo</h1>
-    );
-}
+
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <VaneApp/>
     </React.StrictMode>
 )
