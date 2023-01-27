edf9943f8e8f51b7b7dc016266f65807bf4358e8
Alondra Covarrubias <acovarrubias10@ucol.mx>
2023-01-27 12:20:37 -0600

    Mi primera tarea react

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom/client'
+import { FirstApp } from './FirstApp';
 import { VaneApp } from './VaneApp';
 
 
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <VaneApp/>
+        <FirstApp></FirstApp>
     </React.StrictMode>
 )
