f79478296ac104a2455fdfaafee44af52544be19
Alondra Covarrubias <acovarrubias10@ucol.mx>
2023-01-26 18:26:00 -0600

    Mi primera aplicación en react

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDom from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function App() {
+    //document,createElement......
+    return (<h1>Hola mundo</h1>
+    );
+}
+
+ReactDom.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
